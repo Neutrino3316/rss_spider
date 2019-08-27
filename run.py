bf19077cdab5113e38fffb7248dab18171feb415
Neutrino <1784986256@qq.com>
2019-08-27 17:38:15 +0800

    read config from yml file and use config to fetch

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+import feedparser
 import yaml
 from pymongo import MongoClient
+
+
+def fetch_rss(rss_name: str, rss_link: str, rss_keys_list: list, database):
+
+    rss = feedparser.parse(rss_link)
+
+    for raw_item in rss.entries:
+        save_item = dict((key, raw_item[key]) for key in rss_keys_list)
+        save_item["_id"] = raw_item["id"]
+        update_result = database[rss_name].update_one({'_id': save_item['_id']}, {"$set": save_item}, upsert=True)
+
 
 
 if __name__ == "__main__":
@@ -10,5 +22,9 @@ if __name__ == "__main__":
     print(config)
 
     db_client = MongoClient(config['mongodb']['link'])
+    database = db_client["rss_spider"]
+
+    for key, value in config["rss"].items():
+        fetch_rss(key, value["link"], value["key_list"], database)
 
     db_client.close()
