912ec0ac9343bf55362e669096a25db48ad6a2f1
Neutrino <20875523+Neutrino3316@users.noreply.github.com>
2020-02-04 11:31:25 +0800

    Add test argument. if test, database_name will be rss_spider_test, otherwise it will be rss_spider

--- run.py
+++ run.py
@@ -20,12 +20,14 @@ def get_time_now():
 
 
 class RSS:
-    def __init__(self, rss_name: str, rss_link: str, rss_keys_list: list, database_link):
+
+    def __init__(self, rss_name: str, rss_link: str, rss_keys_list: list, database_link, database_name: str):
         self.wait_time = 1
         self.rss_name = rss_name
         self.rss_link = rss_link
         self.rss_keys_list = rss_keys_list
         self.database_link = database_link
+        self.database_name = database_name
 
         self.rss = None
         self.new_items_count = 0
@@ -52,7 +54,7 @@ class RSS:
     def save_item(self):
         self.new_items_count = 0
         db_client = MongoClient(self.database_link)
-        database = db_client["rss_spider"]
+        database = db_client[self.database_name]
         for save_item in self.save_item_list:
             update_result = database[self.rss_name].update_one({'_id': save_item['_id']}, {"$set": save_item},
                                                                upsert=True)
@@ -91,6 +93,8 @@ if __name__ == "__main__":
     parser.add_argument("--rsshub_host", default=None, type=str,
                         help="the host of RSSHub with http or https, e.g. \
                         https://rsshub.app http://localhost:1200 http://rsshub_diygod:1200")
+    parser.add_argument("--test", action="store_true",
+                        help="if test, write data to database named rss_spider_test.")
     args = parser.parse_args()
 
     if args.mongodb_host is not None:
@@ -99,6 +103,10 @@ if __name__ == "__main__":
     if args.rsshub_host is not None:
         config["rsshub"]["host"] = args.rsshub_host
         print("Using rsshub_host from args, i.e. ", args.rsshub_host)
+    if args.test:
+        database_name = "rss_spider_test"
+    else:
+        database_name = "rss_spider"
 
     for rss_name in config["rss"].keys():
         config["rss"][rss_name]["link"] = config["rss"][rss_name]["link"].replace(
@@ -108,7 +116,7 @@ if __name__ == "__main__":
 
     rss_list = []
     for key, value in config["rss"].items():
-        rss = RSS(key, value["link"], value["key_list"], config['mongodb']['link'])
+        rss = RSS(key, value["link"], value["key_list"], config['mongodb']['link'], database_name)
         rss_list.append(rss)
 
     print(get_time_now(), "all start")
